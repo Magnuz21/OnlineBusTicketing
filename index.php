bfd3de6c53b70f56c9c17310887c6e1779daa270
Aldrin Chicote Abilar <101628771+Magnuz21@users.noreply.github.com>
2023-02-22 12:49:21 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,244 @@
+<?php
+    require 'assets/partials/_functions.php';
+    $conn = db_connect();    
+
+    if(!$conn) 
+        die("Connection Failed");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bus Ticket Fare</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <!-- Font-awesome -->
+    <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- CSS -->
+    <?php 
+        require 'assets/styles/styles.php'
+    ?>
+</head>
+<body>
+    <?php
+    
+    if(isset($_GET["booking_added"]) && !isset($_POST['pnr-search']))
+    {
+        if($_GET["booking_added"])
+        {
+            echo '<div class="my-0 alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Successful!</strong> Booking Added, your PNR is <span style="font-weight:bold; color: #272640;">'. $_GET["pnr"] .'</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+        }
+        else{
+            // Show error alert
+            echo '<div class="my-0 alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Booking already exists
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+        }
+    }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["pnr-search"]))
+    {
+        $pnr = $_POST["pnr"];
+
+        $sql = "SELECT * FROM bookings WHERE booking_id='$pnr'";
+        $result = mysqli_query($conn, $sql);
+
+        $num = mysqli_num_rows($result);
+
+        if($num)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $route_id = $row["route_id"];
+            $customer_id = $row["customer_id"];
+            
+            $customer_name = get_from_table($conn, "customers", "customer_id", $customer_id, "customer_name");
+
+            $customer_phone = get_from_table($conn, "customers", "customer_id", $customer_id, "customer_phone");
+
+            $customer_route = $row["customer_route"];
+            $booked_amount = $row["booked_amount"];
+
+            $booked_seat = $row["booked_seat"];
+            $booked_timing = $row["booking_created"];
+
+            $dep_date = get_from_table($conn, "routes", "route_id", $route_id, "route_dep_date");
+
+            $dep_time = get_from_table($conn, "routes", "route_id", $route_id, "route_dep_time");
+
+            $bus_no = get_from_table($conn, "routes", "route_id", $route_id, "bus_no");
+            ?>
+
+            <div class="alert alert-dark alert-dismissible fade show" role="alert">
+            
+            <h4 class="alert-heading">Ticket Information!</h4>
+            <p>
+                <button class="btn btn-sm btn-success"><a href="assets/partials/_download.php?pnr=<?php echo $pnr; ?>" class="link-light">Download</a></button>
+                <button class="btn btn-danger btn-sm" id="deleteBooking" data-bs-toggle="modal" data-bs-target="#deleteModal" data-pnr="<?php echo $pnr;?>" data-seat="<?php echo $booked_seat;?>" data-bus="<?php echo $bus_no; ?>">
+                    Delete
+                </button>
+            </p>
+            <hr>
+                <p class="mb-0">
+                    <ul class="pnr-details">
+                        <li>
+                            <strong>PNR : </strong>
+                            <?php echo $pnr; ?>
+                        </li>
+                        <li>
+                            <strong>Passenger Name : </strong>
+                            <?php echo $customer_name; ?>
+                        </li>
+                        <li>
+                            <strong>Passenger Phone : </strong>
+                            <?php echo $customer_phone; ?>
+                        </li>
+                        <li>
+                            <strong>Route : </strong>
+                            <?php echo $customer_route; ?>
+                        </li>
+                        <li>
+                            <strong>Bus Number : </strong>
+                            <?php echo $bus_no; ?>
+                        </li>
+                        <li>
+                            <strong>Seat Number : </strong>
+                            <?php echo $booked_seat; ?>
+                        </li>
+                        <li>
+                            <strong>Departure Date : </strong>
+                            <?php echo $dep_date; ?>
+                        </li>
+                        <li>
+                            <strong>Departure Time : </strong>
+                            <?php echo $dep_time; ?>
+                        </li>
+                        <li>
+                            <strong>Issued Timing : </strong>
+                            <?php echo $booked_timing; ?>
+                        </li>
+
+                </p>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php }
+        else{
+            echo '<div class="my-0 alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> Record Doesnt Exist
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+        }
+        
+    ?>
+        
+    <?php }
+
+
+        // Delete Booking
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["deleteBtn"]))
+        {
+            $pnr = $_POST["id"];
+            $bus_no = $_POST["bus"];
+            $booked_seat = $_POST["booked_seat"];
+
+            $deleteSql = "DELETE FROM `bookings` WHERE `bookings`.`booking_id` = '$pnr'";
+
+                $deleteResult = mysqli_query($conn, $deleteSql);
+                $rowsAffected = mysqli_affected_rows($conn);
+                $messageStatus = "danger";
+                $messageInfo = "";
+                $messageHeading = "Error!";
+
+                if(!$rowsAffected)
+                {
+                    $messageInfo = "Record Doesn't Exist";
+                }
+
+                elseif($deleteResult)
+                {   
+                    $messageStatus = "success";
+                    $messageInfo = "Booking Details deleted";
+                    $messageHeading = "Successfull!";
+
+                    // Update the Seats table
+                    $seats = get_from_table($conn, "seats", "bus_no", $bus_no, "seat_booked");
+
+                    // Extract the seat no. that needs to be deleted
+                    $booked_seat = $_POST["booked_seat"];
+
+                    $seats = explode(",", $seats);
+                    $idx = array_search($booked_seat, $seats);
+                    array_splice($seats,$idx,1);
+                    $seats = implode(",", $seats);
+
+                    $updateSeatSql = "UPDATE `seats` SET `seat_booked` = '$seats' WHERE `seats`.`bus_no` = '$bus_no';";
+                    mysqli_query($conn, $updateSeatSql);
+                }
+                else{
+
+                    $messageInfo = "Your request could not be processed due to technical Issues from our part. We regret the inconvenience caused";
+                }
+
+                // Message
+                echo '<div class="my-0 alert alert-'.$messageStatus.' alert-dismissible fade show" role="alert">
+                <strong>'.$messageHeading.'</strong> '.$messageInfo.'
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            }
+    ?>
+
+    
+    <header>
+        <nav>
+            <div>
+                    <a href="#" class="nav-item nav-logo">JAH</a>
+                    <!-- <a href="#" class="nav-item">Gallery</a> -->
+            </div>
+                
+     
+            <div>
+                <a href="#" class="login nav-item" data-bs-toggle="modal" data-bs-target="#loginModal"><i class="fas fa-sign-in-alt" style="margin-right: 0.4rem;"></i>Login</a>
+            
+            </div>
+        </nav>
+    </header>
+    <!-- Login Modal -->
+    <?php require 'assets/partials/_loginModal.php'; 
+        require 'assets/partials/_getJSON.php';
+
+        $routeData = json_decode($routeJson);
+        $busData = json_decode($busJson);
+        $customerData = json_decode($customerJson);
+    ?>
+    
+
+    <section id="home">
+        <div id="route-search-form">
+         
+
+            <center>
+                <button class="btn btn-success " data-bs-toggle="modal" data-bs-target="#loginModal">Administrator Login</button>
+                
+            </center>
+
+            <br>
+            <center>
+            <a href="login.html"><button class="btn btn-warning">Back to User Login </button></a>
+            </center>
+            
+  
+     <!-- Option 1: Bootstrap Bundle with Popper -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <!-- External JS -->
+    <script src="assets/scripts/main.js"></script>
+</body>
+</html>
